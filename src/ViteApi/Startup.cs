006ea6d56f0054a351d36605a756ed6545d6f7a5
joe.chen <joe.chen@championgame.net>
2022-01-21 15:51:38 +0800

    [add] api

--- src/ViteApi/Startup.cs
+++ src/ViteApi/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using ViteApi.Models;
 
 namespace ViteApi
 {
@@ -17,7 +19,7 @@ namespace ViteApi
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddControllers();
-
+      services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
       // services.AddSpaStaticFiles(cfg =>
       // {
       //   cfg.RootPath = "dist";
--- src/vite-client/src/App.vue
+++ src/vite-client/src/App.vue
@@ -3,6 +3,8 @@
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
   <h1>{{ txt }}</h1>
   <h1>{{ counter }}</h1>
+  <button @click="addHandler">add</button>
+  <button @click="clickHandler">click me</button>
 </template>
 
 <script setup lang="ts">
@@ -18,6 +20,26 @@ onMounted(() => {
   txt.value = demoStore.demoStr
   counter.value = demoStore.counter
 })
+
+const clickHandler = () => {
+  fetch('https://localhost:5001/api/todoitems', {
+    method: 'get'
+  })
+    .then(res => res.json().then(data => console.log(data)))
+}
+
+const addHandler = () => {
+  fetch('https://localhost:5001/api/todoitems', {
+    method: 'post',
+    body: JSON.stringify({
+      "name": "walk dog",
+      "isComplete": true
+    }),
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  })
+}
 
 </script>
 
